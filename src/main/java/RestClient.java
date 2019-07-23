d0afc08a1c68a024bffc3ca95ebc0bb181781821
Jagadeesh Kotha <Jkotha@childrensplace.com>
2019-07-23 22:02:53 +0530

    post api method added

--- src/main/java/RestClient.java
+++ src/main/java/RestClient.java
@@ -1,9 +1,12 @@
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +32,20 @@ public class RestClient {
         }
         //HTTP Status code
         CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpGet);//hit the API and get response
+        return closeableHttpResponse;
+    }
+
+    //post method
+    public CloseableHttpResponse postRequest(String uri, String jsonPayLoad, HashMap<String, String> headers) throws UnsupportedEncodingException, IOException {
+        CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(uri); //Http post request
+        httpPost.setEntity(new StringEntity(jsonPayLoad)); //body/payload
+        if (headers.size() != 0) { //checking headers size if not zero
+            for (Map.Entry<String, String> header : headers.entrySet()) { //loop all the headers
+                httpPost.addHeader(header.getKey(), header.getValue()); //add the headers to entry
+            }
+        }
+        CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpPost);
         return closeableHttpResponse;
     }
 }
